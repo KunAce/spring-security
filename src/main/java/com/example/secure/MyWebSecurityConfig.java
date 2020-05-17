c11d87f645ca9ea9402f26b6d62dc214cb5c16a1
KunAce <wxkace@gmail.com>
2020-05-16 19:36:41 -0700

    Added multiple HttpSecurity support

--- src/main/java/com/example/secure/MyWebSecurityConfig.java
+++ src/main/java/com/example/secure/MyWebSecurityConfig.java
@@ -16,7 +16,6 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
